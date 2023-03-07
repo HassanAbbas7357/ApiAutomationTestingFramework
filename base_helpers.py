2e973b20fd27aa2d8d1e651102ebdd0f921ec35b
Hassan-Abbas-SQA <49572299+HassanAbbas7357@users.noreply.github.com>
2023-03-07 17:53:38 +0500

    updated the framework to support Schema Validation as well

--- base_helpers.py
+++ base_helpers.py
@@ -5,24 +5,30 @@ import pytest
 from conftest import *
 import requests
 import json
+import os
+import jsonschema
 
 
+@allure.step("checkInternalServerError")
 def checkInternalServerError(resp):
     print(resp)
     if resp['message'] == 'Internal server error':
         pytest.skip("Skipping Test Case due to Internal Server Error")
 
 
+@allure.step("checkAuthFailureSkipAll")
 def checkAuthFailureSkipAll():
     if Storage.skipAll is True:
         pytest.skip(
             f'Skipping Test Case due to Failed TestCases :{Storage.failedTestCases}')
 
 
+@allure.step("get_randomEmail")
 def get_randomEmail():
     return ''.join(random.choice(string.ascii_letters) for _ in range(15)) + "1421@gmail.com"
 
 
+@allure.step("get_randomUsername")
 def get_randomUsername():
     return ''.join(random.choice(string.ascii_letters) for _ in range(15)) + "1243"
 
@@ -30,3 +36,12 @@ def get_randomUsername():
 @allure.step("Enter Payload{0}")
 def enter_payLoad(payload):
     return payload
+
+
+@allure.step("getSchemafromJSONAndCompareSchema")
+def getSchemafromJSONAndCompareSchema(JsonResp, filename):
+    filepath = os.getcwd()+'/Schemas/'+filename
+    with open(filepath, 'r') as f:
+        schema = json.load(f)
+
+    jsonschema.validate(JsonResp, schema)
